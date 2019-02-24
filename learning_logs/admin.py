0d4d3852bb892078fbfc963e82b12508e15db677
martin-martin <breuss.martin@gmail.com>
2019-02-24 10:42:40 +0800

    register Entry model

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from learning_logs.models import Topic  # or .models for current app
+from learning_logs.models import Topic, Entry  # or .models for current app
 
 # Register your models here.
 admin.site.register(Topic)
+admin.site.register(Entry)
