73315feb1693bfbf6848b34388c7c4ca3fa6489b
martin-martin <breuss.martin@gmail.com>
2019-02-24 10:29:08 +0800

    add Entry model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
-class Topic(models.Model):
+class Topic(models.Model):  # singular turns to Plural in admin
     """A topic the user is learning about."""
     text = models.CharField(max_length=200)  # talk about SQL
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text
+
+
+class Entry(models.Model):
+    """Something specific learned about a Topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)  # show that it needs something set on_delete
+    text = models.TextField()  # let's not limit this one
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return f"{self.text[:50]}..."  # display a clipped version of the entry
 
